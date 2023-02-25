2f9a5ec58184d6b616a69e98ae0c66f706feb80a
Iede Snoek <info@esoxsolutions.nl>
2023-02-25 11:37:46 +0100

    first commit

---
+++ gotut.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	api := router.Group("/api")
+
+	api.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "OK"})
+	})
+
+	router.Run()
+}
